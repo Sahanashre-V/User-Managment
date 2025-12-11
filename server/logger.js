a8de11df2758dd3d8ea5250e2b38839947adb06d
Sahanashre-V <sahanashre.v@gmail.com>
2025-12-11 16:50:21 +0530

    Implemented xss, helmet and logging functionality

---
+++ server/logger.js
@@ -0,0 +1,27 @@
+const { createLogger, transports, format } = require('winston');
+const path = require('path');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.File({
+      filename: path.join(__dirname, 'logs', 'error.log'),
+      level: 'error'
+    }),
+    new transports.File({
+      filename: path.join(__dirname, 'logs', 'app.log')
+    })
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
+}
+
+module.exports = logger;
